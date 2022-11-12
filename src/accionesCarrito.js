6ed3e59d04f46e50be970c6a484d59cef7b83a1c
Rodolfo Schmidt <rodolfoschmidt9@gmail.com>
2022-11-12 12:52:58 -0300

    modificando acciones carrito

--- src/accionesCarrito.js
+++ src/accionesCarrito.js
@@ -72,4 +72,48 @@ const eliminarProductoCarrito = (productoId) => {
     pintarCarrito(carritoActualizado);
 };
 
-export { agregarAlCarrito, validarProductoRepetido, pintarCarrito, eliminarProductoCarrito,};
+//boton de compra final
+const btnCompra = document.getElementById('btn-compra');
+btnCompra.addEventListener('click', () => {
+    swal({
+        title: "esta seguro que quiere realizar la compra?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("perfecto. estamos procesando su pedido", {
+            icon: "warning",
+            
+          })
+          ;
+        } else {
+          swal("Guardaremos tu producto en el carrito");
+        }
+      })});
+
+//botón de vaciar carrito
+
+const btnVaciar = document.getElementById('btn-vaciar');
+btnVaciar.addEventListener('click', () => {
+    swal({
+        title: "esta seguro que quiere eliminar todos los productos?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true,
+      })
+      .then((willDelete) => {
+        if (willDelete) {
+          swal("vaciamos tu carrito", {
+            icon: "warning",
+            
+          })
+          ;
+        }
+      })});
+
+
+
+
+export { agregarAlCarrito, validarProductoRepetido, pintarCarrito, eliminarProductoCarrito, btnCompra, btnVaciar};
